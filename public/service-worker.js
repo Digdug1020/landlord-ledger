e1a2c33c9a9a7aa9e0bca16559069b9fd148bb65
Digdug1020 <mannfordfence@gmail.com>
2026-05-12 04:13:20 -0500

    Fix service worker: network-first, clear all old caches on activate

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = 'landlord-ledger-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/bundle.js',
-];
+const CACHE_NAME = 'landlord-ledger-v2';
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
